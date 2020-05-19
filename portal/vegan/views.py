8982e7e0565c10b7a289833e63f0fa512e0cedf3
XIN LI <xinl21@student.unimelb.edu.au>
2020-05-19 16:35:49 +1000

    web and couchDb ansible

--- portal/vegan/views.py
+++ portal/vegan/views.py
@@ -16,7 +16,6 @@ def home(request):
             # if the end date is earlier than start date, return the error message
 
             # CouchDB authentication
-            print(f)
             username = 'admin'
             password = 'password'
             url = 'http://172.26.132.199:5984/'
@@ -46,15 +45,30 @@ def home(request):
                          'region_name': ['Melbourne - Inner', 'Melbourne - Inner East', 'Melbourne - Inner South',
                                          'Melbourne - North East', 'Melbourne - North West', 'Melbourne - Outer East',
                                          'Melbourne - South East', 'Melbourne - West', 'Mornington Peninsula']}
+            region_label = code_name['region_name']
+
             code_name = pd.DataFrame(code_name)
             edu_table = edu_table.merge(code_name, left_on='code', right_on='code')
             edu_twitter = edu_table.merge(senti_by_region, left_on='code', right_on='code')
-            print(edu_twitter)
+            # print(edu_twitter)
+            # print(region_label)
+            tertiary_list = [edu_twitter.iloc[0, 1],edu_twitter.iloc[1, 1], edu_twitter.iloc[2, 1],
+                             edu_twitter.iloc[3, 1],edu_twitter.iloc[4, 1], edu_twitter.iloc[5, 1],
+                             edu_twitter.iloc[6, 1],edu_twitter.iloc[7, 1], edu_twitter.iloc[8, 1]]
+            print(tertiary_list)
 
-            return render(request, 'vegan/home.html')
+            senti_list = [edu_twitter.iloc[0, 3],edu_twitter.iloc[1, 3], edu_twitter.iloc[2, 3],
+                             edu_twitter.iloc[3, 3],edu_twitter.iloc[4, 3], edu_twitter.iloc[5, 3],
+                             edu_twitter.iloc[6, 3],edu_twitter.iloc[7, 3], edu_twitter.iloc[8, 3]]
+            print(senti_list)
+            tweet_count = [edu_twitter.iloc[0, 4], edu_twitter.iloc[1, 4], edu_twitter.iloc[2, 4],
+                          edu_twitter.iloc[3, 4], edu_twitter.iloc[4, 4], edu_twitter.iloc[5, 4],
+                          edu_twitter.iloc[6, 4], edu_twitter.iloc[7, 4], edu_twitter.iloc[8, 4]]
+            print(tweet_count)
+            args = {'featureform': featureform, 'tertiary_list': tertiary_list, 'senti_list': senti_list, 'tweet_count': tweet_count, 'region_label': region_label}
+            return render(request, 'vegan/home.html', args)
 
     featureform = featureSelectionForm()
-
     return render(request, 'vegan/home.html', {'featureform': featureform})
 
 
@@ -67,7 +81,7 @@ def income(request):
             # if the end date is earlier than start date, return the error message
 
             # CouchDB authentication
-            print(f)
+            # print(f)
             username = 'admin'
             password = 'password'
             url = 'http://172.26.132.199:5984/'
@@ -100,8 +114,8 @@ def income(request):
             code_name = pd.DataFrame(code_name)
             income_table = income_table.merge(code_name, left_on='code', right_on='code')
             income_twitter = income_table.merge(senti_by_region, left_on='code', right_on='code')
-            print(income_twitter)
-
+            # print(income_twitter)
+            print(income_twitter.iloc[1, 4])
             return render(request, 'vegan/income.html')
 
     featureform = featureSelectionForm()
