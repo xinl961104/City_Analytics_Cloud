d5e35e1f383cf79d1c4e509672e14c4669d98971
XIN LI <xinl21@student.unimelb.edu.au>
2020-05-22 17:27:03 +1000

    haproxy playbook

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zqul2yb($jzqqr)+y+2ji7*_x&o_=8%mvsplet9bs_@#maw45@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
